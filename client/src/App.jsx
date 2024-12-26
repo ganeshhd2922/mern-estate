ce5d101a888b42a6c8a7719a3ba0e018fad47db2
Ganesh Dhakarke <100dhakarkeganesh@gmail.com>
2024-12-26 09:50:43 +0530

    create and run the server

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+        <Header/>
+            <Routes>
+               
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+                
+                {/* Redirect any undefined routes to the home page */}
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function App() {
-  return (
-    <span className='text-red-500'>hello</span>
-  )
-}
